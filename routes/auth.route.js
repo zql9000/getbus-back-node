1d8a10aee991dbc7e486710e87215634b8cae8c8
Ezequiel López <lelopez@indracompany.com>
2021-03-26 18:38:55 -0300

    Add authentication (login anda renew token)

---
+++ routes/auth.route.js
@@ -0,0 +1,26 @@
+/*
+    Rutas de usuarios/auth
+    /api/auth
+*/
+const express = require('express');
+const router = express.Router();
+const { check } = require('express-validator');
+const { validateJWT } = require('../middlewares/validate-jwt');
+const { validateParams } = require('../middlewares/validate-params');
+const { login, renewToken } = require('../controllers/auth.controller');
+
+router.post(
+  '/',
+  [
+    check('username', 'Username is required').not().isEmpty(),
+    check('password', 'Password must be at least 8 characters').isLength({
+      min: 8,
+    }),
+    validateParams,
+  ],
+  login
+);
+
+router.get('/renew', [validateJWT], renewToken);
+
+module.exports = router;
